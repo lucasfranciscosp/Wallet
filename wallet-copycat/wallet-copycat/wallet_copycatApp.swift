5bf74532257d290b48e601b121bfb622868fb60d
Gabriel Gandur <grgandur@gmail.com>
2023-05-26 10:29:45 -0300

    Criando projeto

---
+++ wallet-copycat/wallet-copycat/wallet_copycatApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallet_copycatApp.swift
+//  wallet-copycat
+//
+//  Created by Gabriel Gandur on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct wallet_copycatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
