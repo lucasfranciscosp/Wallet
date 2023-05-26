9d68307791cda66881529d663839627779ea786a
Caio Piteli <caiogpiteli@gmail.com>
2023-05-26 12:01:33 -0300

    Sem DS

--- wallet-copycat/wallet-copycat/ContentView.swift
+++ wallet-copycat/wallet-copycat/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("ola mundo!")
+            Text("ola mundo!!!")
         }
         .padding()
     }
